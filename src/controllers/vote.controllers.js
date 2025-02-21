8e7617592589d152dae834821d78a3b35385d329
yash <yash162534@gmail.com>
2025-02-21 13:29:15 +0530

    votes fixed

--- src/controllers/vote.controllers.js
+++ src/controllers/vote.controllers.js
@@ -3,58 +3,81 @@ import {Community} from "../models/community.model.js"
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { ApiError } from "../utils/apiError.js";
-
-
+import { User } from "../models/user.model.js";
 
 
 const upvotePost = asyncHandler(async (req, res) => {
     const post = await Post.findById(req.params.id);
-
     if (!post) {
-        throw new ApiError(404,"post not found") 
+        throw new ApiError(404, "Post not found");
     }
 
-    if (!post.upvotedBy.includes(req.user._id)) {
+    const user = await User.findById(req.user._id);
+
+    if (!user.upvotedPosts.includes(post._id)) {
+        user.upvotedPosts.push(post._id);
         post.upvotedBy.push(req.user._id);
+
+        user.downvotedPosts = user.downvotedPosts.filter(id => !id.equals(post._id));
+        post.downvotedBy = post.downvotedBy.filter(id => !id.equals(req.user._id));
+    } else {
+        user.upvotedPosts = user.upvotedPosts.filter(id => !id.equals(post._id));
+        post.upvotedBy = post.upvotedBy.filter(id => !id.equals(req.user._id));
+    }
+
+    await user.save();
+    await post.save();
+
+    return res.status(200).json(new ApiResponse(200, "Post upvoted", { post, user }));
+});
+
+
+const downvotePost = asyncHandler(async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+        throw new ApiError(404, "Post not found");
+    }
+
+    const user = await User.findById(req.user._id);
+
+    if (!user.downvotedPosts.includes(post._id)) {
+        user.downvotedPosts.push(post._id);
+        post.downvotedBy.push(req.user._id);
+        user.upvotedPosts = user.upvotedPosts.filter(id => !id.equals(post._id));
+        post.upvotedBy = post.upvotedBy.filter(id => !id.equals(req.user._id));
+    } else {
+        user.downvotedPosts = user.downvotedPosts.filter(id => !id.equals(post._id));
         post.downvotedBy = post.downvotedBy.filter(id => !id.equals(req.user._id));
     }
 
+    await user.save();
     await post.save();
-    return res.status(200).json(new ApiResponse(200, 'Post upvoted', post));
+
+    return res.status(200).json(new ApiResponse(200, "Post downvoted", { post, user }));
 });
 
-const downvotePost=asyncHandler(async(req,res)=>{
-    const post=await Post.findById(req.params.id)
+
+// not for use
+const deleteUpvotePost = asyncHandler(async (req, res) => {
+    const post = await Post.findById(req.params.id);
     if (!post) {
-        throw new ApiError(404,"post not found") 
+        throw new ApiError(404, "Post not found");
     }
 
-    if(!post.downvotedBy.includes(req.user._id)){
-        post.downvotedBy.push(req.user._id);
-        post.upvotedBy=post.upvotedBy.filter(id=>!id.equals(req.user._id))
+    const userIndex = post.upvotedBy.findIndex(id => id.equals(req.user._id));
+    if (userIndex === -1) {
+        return res.status(400).json(new ApiResponse(400, "User has not upvoted this post", post));
     }
 
-    await post.save();
-    return res.status(200).json(new ApiResponse(200, 'Post downvoted', post));
+    post.upvotedBy.splice(userIndex, 1);
 
-
-})
-
-const deleteupvote= asyncHandler(async(req,res)=>{
-    const post= await Post.findById(req.params.id);
-    if (!post) {
-        throw new ApiError(404,"post not found") 
+    try {
+        await post.save();
+        return res.status(200).json(new ApiResponse(200, "Upvote removed", post));
+    } catch (error) {
+        throw new ApiError(500, "Failed to update post");
     }
-
-    if(!post.downvotedBy.includes(req.user._id)){
-        post.downvotedBy.push(req.user._id);
-        post.upvotedBy=post.upvotedBy.filter(id=>!id.equals(req.user._id))
-    }
-
-    await post.save();
-    return res.status(200).json(new ApiResponse(200, 'Post downvoted', post));
-
-})
+});
 
 
 const deleteDownvotePost = asyncHandler(async (req, res) => {
@@ -63,42 +86,16 @@ const deleteDownvotePost = asyncHandler(async (req, res) => {
         throw new ApiError(404, "Post not found");
     }
 
-    // Check if the user is actually in the downvotedBy list
     const userIndex = post.downvotedBy.findIndex(id => id.equals(req.user._id));
     if (userIndex === -1) {
-        return res.status(400).json(new ApiResponse(400, 'User has not downvoted this post', post));
+        return res.status(400).json(new ApiResponse(400, "User has not downvoted this post", post));
     }
 
-    // Remove user from the downvotedBy list
     post.downvotedBy.splice(userIndex, 1);
 
     try {
         await post.save();
-        return res.status(200).json(new ApiResponse(200, 'Downvote removed', post));
-    } catch (error) {
-        throw new ApiError(500, "Failed to update post");
-    }
-});
-
-
-const deleteUpvotePost = asyncHandler(async (req, res) => {
-    const post = await Post.findById(req.params.id);
-    if (!post) {
-        throw new ApiError(404, "Post not found");
-    }
-
-    // Check if the user is actually in the downvotedBy list
-    const userIndex = post.upvotedBy.findIndex(id => id.equals(req.user._id));
-    if (userIndex === -1) {
-        return res.status(400).json(new ApiResponse(400, 'User has not downvoted this post', post));
-    }
-
-    // Remove user from the downvotedBy list
-    post.downvotedBy.splice(userIndex, 1);
-
-    try {
-        await post.save();
-        return res.status(200).json(new ApiResponse(200, 'Downvote removed', post));
+        return res.status(200).json(new ApiResponse(200, "Downvote removed", post));
     } catch (error) {
         throw new ApiError(500, "Failed to update post");
     }
