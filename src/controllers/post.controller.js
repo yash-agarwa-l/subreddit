c3b84c5b11b56387970a5ec925b07de4a6ebf360
yash <yash162534@gmail.com>
2025-02-22 19:09:54 +0530

    hashing implemented

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -6,6 +6,7 @@ import { ApiError } from "../utils/apiError.js";
 import { s3Client } from "./picures.controllers.js";
 import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import crypto from 'crypto';
 
  const createPost = asyncHandler(async (req, res) => {
     const { title, description, postType, communityId, imageKeys, isAnonymous } = req.body;
@@ -167,3 +168,107 @@ export {
     deletePost,
     getGenericPosts
 }
+
+
+
+const algorithm = 'aes-256-ctr'; 
+const secretKey = process.env.SECRET_KEY; 
+const iv = crypto.randomBytes(16); 
+
+const encrypt = (text) => {
+    const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey, 'utf-8'), iv);
+    let encrypted = cipher.update(text, 'utf-8', 'hex');
+    encrypted += cipher.final('hex');
+    return { iv: iv.toString('hex'), encryptedData: encrypted }; 
+};
+
+const decrypt = (encryptedText, iv) => {
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey, 'utf-8'), Buffer.from(iv, 'hex'));
+    let decrypted = decipher.update(encryptedText, 'hex', 'utf-8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+};
+
+
+
+const createPostEncrpted = asyncHandler(async (req, res) => {
+    const { title, description, postType, communityId, imagekey, isAnonymous } = req.body;
+    const userId = req.user._id;
+
+    if (!title || !description || !postType) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    let encryptedTitle, encryptedDescription, iv;
+
+    if (isAnonymous) {
+        const encryptedTitleObj = encrypt(title);
+        const encryptedDescriptionObj = encrypt(description);
+        
+        encryptedTitle = encryptedTitleObj.encryptedData;
+        encryptedDescription = encryptedDescriptionObj.encryptedData;
+        iv = encryptedTitleObj.iv;  
+    }
+
+    const community = await Community.findById(communityId);
+    if (!community) {
+        throw new ApiError(404, "Community not found");
+    }
+
+    let imageUrl = null;
+
+    if (imagekey) {
+        try {
+            const getCommand = new GetObjectCommand({
+                Bucket: process.env.BUCKET_NAME,
+                Key: `uploads/userUploads/${imagekey}`,
+            });
+
+            imageUrl = await getSignedUrl(s3Client, getCommand, { expiresIn: 3600 });
+
+            await User.findByIdAndUpdate(userId, { $set: { profileImage: imageUrl } });
+        } catch (error) {
+            throw new ApiError(500, "Error generating signed URL for image");
+        }
+    }
+
+    const post = await Post.create({
+        title: isAnonymous ? encryptedTitle : title,
+        description: isAnonymous ? encryptedDescription : description,
+        author: userId,
+        community: communityId,
+        isAnonymous: isAnonymous,
+        iv: iv,  
+        imageUrl: imageUrl,
+    });
+
+    return res.status(201).json(
+        new ApiResponse(201, "Post created", {
+            post,
+            signedImage: {
+                fileName: imagekey,
+                url: imageUrl,
+            },
+        })
+    );
+});
+
+
+const getPostByIdEnc = asyncHandler(async (req, res) => {
+    const post = await Post.findById(req.params.id)
+        .populate('author', 'username')
+        .populate('community', 'name');
+
+    if (!post) {
+        throw new ApiError(404, "Post not found");
+    }
+    if (post.isAnonymous) {
+        post.title = decrypt(post.title, post.iv);
+        post.description = decrypt(post.description, post.iv);
+        delete post.author; 
+    }
+
+    
+
+    return res.status(200).json(new ApiResponse(200, 'Post fetched successfully', post));
+});
